28c7b5ba781a82cfeb665ed62ff2fe57eebd19c2
Ruslan Khalikov <rus-h-903@mail.ru>
2024-03-07 19:18:52 +0300

    apply categories test

--- backend/src/test_calculator.py
+++ backend/src/test_calculator.py
@@ -10,6 +10,12 @@ class TestCalculator(unittest.TestCase):
     def test_add(self):
         self.assertEqual(self.calculator.addition(1, 2), 3)
 
+    def test_mul(self):
+        self.assertEqual(self.calculator.addition(1, 2), 2)
+
+    def test_sub(self):
+        self.assertEqual(self.calculator.addition(1, 2), 2)
+
 
 if __name__ == "__main__":
     unittest.main()
