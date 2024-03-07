e14acb0166f429ebb66ed9a33e13b1defddeadaa
Ruslan Khalikov <rus-h-903@mail.ru>
2024-03-07 19:27:44 +0300

    coverage 91%

--- backend/src/test_calculator.py
+++ backend/src/test_calculator.py
@@ -11,10 +11,28 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(self.calculator.addition(1, 2), 3)
 
     def test_mul(self):
-        self.assertEqual(self.calculator.addition(1, 2), 2)
+        self.assertEqual(self.calculator.multiplication(1, 2), 2)
 
     def test_sub(self):
-        self.assertEqual(self.calculator.addition(1, 2), 2)
+        self.assertEqual(self.calculator.subtraction(2, 2), 0)
+
+    def test_div(self):
+        self.assertEqual(self.calculator.division(2, 2), 1)
+
+    def test_abs(self):
+        self.assertEqual(self.calculator.adsolute(-2), 2)
+
+    def test_degree(self):
+        self.assertEqual(self.calculator.degree(2, 2), 4)
+
+    # def test_ln(self):
+    #     self.assertEqual(self.calculator.ln(0), 1)
+    #
+    # def test_log(self):
+    #     self.assertEqual(self.calculator.log(2, 0), 1)
+
+    def test_sqrt(self):
+        self.assertEqual(self.calculator.sqrt(1), 1)
 
 
 if __name__ == "__main__":
